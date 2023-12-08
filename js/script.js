93eb930d1ac2581d7dbe98b6644aceda9fa32552
hangeunyoung <150780612+hangeunyoung@users.noreply.github.com>
2023-12-08 13:25:03 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,25 +1,25 @@
 let depth1_li = document.querySelectorAll('header nav .depth1 > li')
-for(let i = 0; i < depth1_li.length; i++){
-    depth1_li[i].addEventListener('mouseover', function(){
+for (let i = 0; i < depth1_li.length; i++) {
+    depth1_li[i].addEventListener('mouseover', function () {
         // for(let i = 0; i < depth1_li.length; i++) {
         //     depth1_li[i].classList.add('show');
         // }
-        depth1_li.forEach(function(depth1) {
+        depth1_li.forEach(function (depth1) {
             depth1.classList.add('show')
         });
-        
+
         console.log('완성');
     });
 }
-for(let i = 0; i < depth1_li.length; i++){
-    depth1_li[i].addEventListener('mouseout', function(){
+for (let i = 0; i < depth1_li.length; i++) {
+    depth1_li[i].addEventListener('mouseout', function () {
         // for(let i = 0; i < depth1_li.length; i++) {
         //     depth1_li[i].classList.add('show');
         // }
-        depth1_li.forEach(function(depth1) {
+        depth1_li.forEach(function (depth1) {
             depth1.classList.remove('show')
         });
-        
+
         console.log('완성');
     });
 }
@@ -27,7 +27,7 @@ for(let i = 0; i < depth1_li.length; i++){
 const faqs = document.querySelectorAll(".faq");
 
 faqs.forEach((faq) => {
-    faq.addEventListener("click", ()=>{
+    faq.addEventListener("click", () => {
         faq.classList.toggle("active");
     });
 });
@@ -35,19 +35,29 @@ faqs.forEach((faq) => {
 
 let imgBox = document.querySelector('main .slider .slider-box');
 let img = document.querySelectorAll('.slider-box .title').length;
-let count = 0; 
-let imgWidth = -100; 
-
-setInterval(function(){
+let count = 0;
+let imgWidth = -100;
+console.log(img)
+setInterval(function () {
     count++
-    if(count > img -1){count = 0};  
+    if (count > img - 1) { count = 0 };
     // console.log(count * imgWidth)
     imgBox.style.transform = `translate(${count * imgWidth}vw, 0)`;
-}, 10000);
+}, 20000);
 
 let next = document.querySelector('main .main-banner .next-btn');
 let prev = document.querySelector('.prev-btn');
 
-next.addEventListener('click', function(){
-    img.style.transform = "translate(-100vw)"
+next.addEventListener('click', function () {
+    count++;
+    if (count > img - 1) { count = 0 };
+    console.log(count);
+
+    document.querySelector('main .slider .slider-box').style.transform = `translate(${-100 * count}vw, 0)`
+});
+prev.addEventListener('click', function () {
+    count--;
+    if (count < 0) { count = 2 };
+    console.log(count);
+    document.querySelector('main .slider .slider-box').style.transform = `translate(${-100 * count}vw, 0)`
 });
